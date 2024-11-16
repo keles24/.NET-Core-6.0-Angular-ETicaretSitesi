b31aae7e11d35816011bafc1674b2cc4fd60f832
Emircan Keleş <emircan.keles@femasmetalsanayi.onmicrosoft.com>
2024-11-17 00:04:07 +0300

    15.11.24 2 ve 3. video

---
+++ Insfrastructure/ETicaretAPI.Persistence/ServiceRegistration.cs
@@ -0,0 +1,19 @@
+﻿using ETicaretAPI.Application.Abstractions;
+using ETicaretAPI.Persistence.Concreates;
+using Microsoft.Extensions.DependencyInjection;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ETicaretAPI.Persistence
+{
+    public static class ServiceRegistration
+    {
+        public static void AddPersistanceServices(this IServiceCollection services)
+        {
+            services.AddSingleton<IProductService, IProductService>();
+        }
+    }
+}
---
+++ Presentation/ETicaretAPI.API/Controllers/ProductController.cs
@@ -0,0 +1,15 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace ETicaretAPI.API.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class ProductController : ControllerBase
+    {
+        public IActionResult GetProducts()
+        {
+            return Ok();
+        }
+    }
+}
